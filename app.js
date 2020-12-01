39ddb5a36e195cd5d802452af21bc03137b14d3e
Stephen Cantoria <kstv@yahoo.com>
2020-12-01 13:15:50 -0600

    sessions part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -33,7 +35,17 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser("12345-67890-09876-54321"));
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 //
 // Basic YWRtaW46cGFzc3dvcmQ=
@@ -41,8 +53,9 @@ app.use(cookieParser("12345-67890-09876-54321"));
 // ['admin', 'password']
 //
 function auth(req, res, next) {
-  //console.log(req.headers);
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error("You are not authenticated!");
@@ -57,7 +70,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
-      res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
       return next(); // authorized
     } else {
       const err = new Error("You are not authenticated!");
@@ -66,7 +79,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
